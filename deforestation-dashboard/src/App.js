195e09b7fda63576c5f5b9381d44dc455a22e934
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-27 11:46:23 -0500

    added tab navigation to app and removed default styling

--- deforestation-dashboard/src/App.js
+++ deforestation-dashboard/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import TabNav from './Components/TabNav';
 import { Route } from 'react-router-dom';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
+// import Login from './Components/Login';
+// import Signup from './Components/Signup';
 
 
 import './App.css';
@@ -11,15 +11,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       DEFORESTATION DASHBOARD
-       {/* <TabNav /> */}
-      </header>
+        Deforestation Dashboard
+      <TabNav />
+     </header>
 
-      <Route path='/' component={Home} />
+      {/* <Route path='/' component={Home} />
       <Route path='/country' component={country} />
-      <Route path='/savedCharts' component={saved} />
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Signup} />
+      <Route path='/savedCharts' component={saved} /> */}
+      {/* <Route path='/login' component={Login} />
+      <Route path='/register' component={Signup} /> */}
     </div>
   );
 }
