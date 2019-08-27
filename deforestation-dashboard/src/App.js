2ba3871d79fc44589e2a601d81cc51e846fa78c8
allisonkydy <allisnelly@gmail.com>
2019-08-27 16:27:09 -0500

    refactored TavNav to handle routes, added main route and welcome page

--- deforestation-dashboard/src/App.js
+++ deforestation-dashboard/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Welcome from './components/Welcome';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import CountryList from './components/CountryList';
+import SavedPublic from './components/SavedPublic';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={Home} />
-      {/* <Route path='/country' component={country} /> */}
-      {/* <Route path='/saved' component={saved} /> */}
+      <Route exact path='/' component={Welcome}/>
+      <Route path='/main' component={Home} />
+      <Route path='/main/country-list' component={CountryList} />
+      <Route path='/main/saved' component={SavedPublic} />
+      {/* <Route path='/country-list' />
+      <Route path='/saved' /> */}
       <Route path='/login' component={Login} />
       <Route path='/register' component={Signup} />
     </div>
