21a07fcc5eed6823f9d78cb6d3522cb3d82355a2
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-28 17:42:21 -0500

    saved merge resolutions

--- deforestation-dashboard/src/components/Logo.js
+++ deforestation-dashboard/src/components/Logo.js
@@ -3,11 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Logo() {
-<<<<<<< HEAD
-   
-=======
-
->>>>>>> e4265e27ff53dfeb0aeaadac87cbbbdf1595db6b
    return (
       <Link to='/map-view' className="logo">
          <h1>
