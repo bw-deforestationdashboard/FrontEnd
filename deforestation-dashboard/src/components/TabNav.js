186e816faa9f8cbb4d40998fefc49a98c3dc036d
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-28 14:16:08 -0500

    resolved conflicts from latest pull

---
+++ deforestation-dashboard/src/components/TabNav.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Menu } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import { Link, Route } from 'react-router-dom';
+
+import MapView from './MapView';
+import CountryView from './CountryView';
+import SavedPublic from './SavedPublic';
+
+function TabNav(props) {
+  const [activeItem, setActiveItem ] = useState(props.location.pathname);
+
+  const handleItemClick = (e, {name}) => {
+    setActiveItem(name);
+  }
+
+  useEffect(() => {
+    if (activeItem !== props.location.pathname) {
+      setActiveItem(props.location.pathname)
+    }
+  }, [props.location.pathname])
+
+  return (
+    <>
+      <Menu tabular>
+        <Menu.Item 
+          as={ Link }
+          to='/map-view'
+          name='/map-view'
+          active={activeItem === '/map-view'}
+          onClick={handleItemClick}
+        >
+          Map View
+        </Menu.Item>
+        <Menu.Item
+          as={ Link }
+          to='/country-view'
+          name='/country-view'
+          active={activeItem === '/country-view'}
+          onClick={handleItemClick}
+        >
+          Country View
+        </Menu.Item>
+        <Menu.Item
+          as={ Link }
+          to='/saved'
+          name='/saved'
+          active={activeItem === '/saved'}
+          onClick={handleItemClick}
+        >
+          Saved Charts
+        </Menu.Item>
+      </Menu>
+      <Route path='/map-view' component={MapView} />
+      <Route path='/country-view' component={CountryView} />
+      <Route path='/saved' component={SavedPublic} />
+    </>
+)
+}
+
+export default TabNav
